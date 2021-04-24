acfbf97083e5edfcda14eb8d4f64132db14b5276
yanzt <yanzt@gistack.cn>
2021-04-24 16:41:32 +0800

    创建reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,60 @@
-// 请从课程简介里下载本代码
-import React, {useState, useContext} from 'react'
+import React, { useContext, useState } from 'react'
 
 const appContext = React.createContext(null)
-export const App = () => {
-  const [appState, setAppState] = useState({
-    user: {name: 'frank', age: 18}
-  })
-  const contextValue = {appState, setAppState}
-  return (
-    <appContext.Provider value={contextValue}>
-      <大儿子/>
-      <二儿子/>
-      <幺儿子/>
-    </appContext.Provider>
-  )
-}
-const 大儿子 = () => <section>大儿子<User/></section>
-const 二儿子 = () => <section>二儿子<UserModifier/></section>
-const 幺儿子 = () => <section>幺儿子</section>
-const User = () => {
-  const contextValue = useContext(appContext)
-  return <div>User:{contextValue.appState.user.name}</div>
 
+
+const reducer = (state, { type, payload }) => {
+    if (type === 'updateUser') {
+        return {
+            ...state,
+            user: {
+                ...state.user,
+                ...payload
+            }
+        }
+    } else {
+        return state
+    }
 }
-const UserModifier = () => {
-  const {appState, setAppState} = useContext(appContext)
-  const onChange = (e) => {
-    appState.user.name = e.target.value
-    setAppState({...contextValue.appState})
-  }
-  return <div>
-    <input value={contextValue.appState.user.name}
-      onChange={onChange}/>
-  </div>
+
+
+export const App = () => {
+    const [appState, setAppState] = useState({
+        user: { name: '张三', age: 123 }
+    })
+    const contextValue = { appState, setAppState };
+    return (
+        <appContext.Provider value={contextValue}>
+            <A1></A1>
+            <A2></A2>
+            <A3></A3>
+        </appContext.Provider>
+    )
+}
+
+const A1 = () => {
+    const { appState } = useContext(appContext);
+    return (
+        <h1>USER: {appState.user.name}</h1>
+    )
+}
+
+
+const A2 = () => {
+    const { appState, setAppState } = useContext(appContext);
+    const onChange = (e) => {
+        setAppState(reducer(appState, { type: 'updateUser', payload: { name: e.target.value } }))
+    }
+    return (
+        <input
+            value={appState.user.name}
+            onChange={onChange}
+        ></input>
+    )
+}
+const A3 = () => {
+    return (
+        <h1>A3</h1>
+    )
 }
 
