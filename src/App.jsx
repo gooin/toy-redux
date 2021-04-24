64310a3d84f7ac60cf1cec1f633b0746e6833d52
yanzt <yanzt@gistack.cn>
2021-04-24 17:06:01 +0800

    dispatch规范setState的流程

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React, { useContext, useState } from 'react'
+import React, {useContext, useState} from 'react'
 
 const appContext = React.createContext(null)
 
 
-const reducer = (state, { type, payload }) => {
+// reducer 规范state的更新流程
+const reducer = (state, {type, payload}) => {
     if (type === 'updateUser') {
         return {
             ...state,
@@ -18,38 +19,50 @@ const reducer = (state, { type, payload }) => {
 }
 
 
+
+
 export const App = () => {
     const [appState, setAppState] = useState({
-        user: { name: '张三', age: 123 }
+        user: {name: '张三', age: 123}
     })
-    const contextValue = { appState, setAppState };
+    const contextValue = {appState, setAppState};
     return (
         <appContext.Provider value={contextValue}>
-            <A1></A1>
-            <A2></A2>
-            <A3></A3>
+            <A1/>
+            <Wrapper/>
+            <A3/>
         </appContext.Provider>
-    )
+    );
 }
 
 const A1 = () => {
-    const { appState } = useContext(appContext);
+    const {appState} = useContext(appContext);
     return (
         <h1>USER: {appState.user.name}</h1>
     )
 }
 
 
-const A2 = () => {
-    const { appState, setAppState } = useContext(appContext);
+const Wrapper = ()=>{
+    const {appState, setAppState} = useContext(appContext);
+
+    // 使用dispatch规范setState流程
+    const dispatch = (action) => {
+        setAppState(reducer(appState,action))
+    }
+    return <A2 dispatch={dispatch} appState={appState}/>
+}
+
+
+const A2 = ({dispatch,appState}) => {
     const onChange = (e) => {
-        setAppState(reducer(appState, { type: 'updateUser', payload: { name: e.target.value } }))
+        dispatch({type: 'updateUser', payload: {name: e.target.value}})
     }
     return (
         <input
-            value={appState.user.name}
-            onChange={onChange}
-        ></input>
+    value={appState.user.name}
+    onChange={onChange}
+    />
     )
 }
 const A3 = () => {
